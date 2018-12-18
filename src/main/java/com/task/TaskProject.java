2ea3b2345398a9cafad0a339a5340b0aa20c052c
MayorSmart <lastborn32@gmail.com>
2018-12-18 12:29:21 +0100

    basic task assign app completed

--- src/main/java/com/task/TaskProject.java
+++ src/main/java/com/task/TaskProject.java
@@ -1,12 +1,25 @@
 package com.task;
 
+import com.task.entities.User;
+import com.task.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskProject {
+public class TaskProject implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(TaskProject.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User newAdmin = new User("admin@mail.com", "Admin", "123456");
+        userService.createAdmin(newAdmin);
     }
 }
